01943a06d1697949901d1f93844acd569f23544c
Pedro Henrique <pedrohenriquedasilvapereira12@gmail.com>
2025-04-08 23:28:20 -0300

    feat: add separeted handler and personalized logger

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "github.com/pedrodese/gopportunities/router"
+import (
+	"github.com/pedrodese/gopportunities/config"
+	"github.com/pedrodese/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
 
-	//Initialize Router
+	logger := *config.GetLogger("main")
 
+	//Initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initiliazation error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 
 }
