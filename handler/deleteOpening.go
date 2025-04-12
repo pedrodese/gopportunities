e21bde93fe90c09fc6ef88dd6c6cd4bd1aa25111
Pedro Henrique <pedrohenriquedasilvapereira12@gmail.com>
2025-04-12 00:45:56 -0300

    Add the logic for the delete handler, list handler and show opening handler.

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,29 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pedrodese/gopportunities/schemas"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"response": "GET opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamsIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	//Find opening by ID
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+	sendSuccess(ctx, "delete-opening", opening)
+
 }
--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,23 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pedrodese/gopportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"response": "GET opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, fmt.Sprintf("opening with id: %s not found", id))
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error finding a opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
